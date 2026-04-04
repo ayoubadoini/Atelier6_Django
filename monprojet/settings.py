dd2d4135ceb36a76e5660f5b6c409e8ee8518bdd
ayoubadoini <ayoubadoini1994@gmail.com>
2026-04-04 22:56:31 +0200

    Disable AutoMigrateMiddleware - use manual migrations instead

--- monprojet/settings.py
+++ monprojet/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'monprojet.middleware.AutoMigrateMiddleware',  # Apply migrations on first request
+    # 'monprojet.middleware.AutoMigrateMiddleware',  # Disabled - use manual migrations instead
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
